31bed49214f0d52640eb18bff74e116b176ef84e
kate <kate@dariox.club>
2023-01-12 10:36:34 +0800

    cleanup

--- firefox/Profiles/[profilenamehere]/printaddons.js
+++
@@ -1,2 +0,0 @@
-const addons = require("./addons.json").addons
-addons.forEach(a => a.name != undefined ? console.log(`${a.type}: ${a.name}@${a.version}`) : null )
--- install-linux.sh
+++ install-linux.sh
@@ -30,7 +30,14 @@ _install_dnf() {
         curl \
         git \
         zsh \
-        micro
+        nano \
+        flatpak
+	if [ -f "ocs-url-3.1.0-1.x86_64.rpm" ]; then
+		rm ocs-url-3.1.0-1.x86_64.rpm
+	fi
+	wget https://res.kate.pet/upload/e241d69e-d407-45f2-a160-25f28e8c7462/ocs-url-3.1.0-1.x86_64.rpm
+	sudo dnf install ./ocs-url-3.1.0-1.x86_64.rpm -y
+	rm ocs-url-3.1.0-1.x86_64.rpm
 }
 _install_apt() {
     sudo apt update
@@ -41,7 +48,55 @@ _install_apt() {
         curl \
         git \
         zsh \
-        micro
+        nano \
+        flatpak
+	sudo apt install -y \
+		libqt5svg5 \
+		qml-module-qtquick-controls
+	if [ -f "ocs-url_3.1.0-0ubuntu1_amd64.deb" ]; then
+		rm ocs-url_3.1.0-0ubuntu1_amd64.deb
+	fi
+	wget https://res.kate.pet/upload/bbbb2a37-8265-4d5e-98c2-74dd85638186/ocs-url_3.1.0-0ubuntu1_amd64.deb
+	sudo apt install ./ocs-url_3.1.0-0ubuntu1_amd64.deb -y
+	rm ocs-url_3.1.0-0ubuntu1_amd64.deb
+}
+_install_flatpak() {
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak install -y \
+		com.github.phase1geo.minder \
+		com.github.artemanufrij.regextester \
+		com.github.childishgiant.mixer \
+		com.github.debauchee.barrier \
+		com.github.iwalton3.jellyfin-media-player \
+		com.github.micahflee.torbrowser-launcher \
+		com.github.arshubham.cipher  \
+		app.drey.Warp \
+		chat.revolt.RevoltDesktop \
+		com.dec05eba.gpu_screen_recorder \
+		com.discordapp.Discord \
+		com.github.LongSoft.UEFITool \
+		com.visualstudio.code \
+		com.vysp3r.ProtonPlus \
+		com.whitemagicsoftware.kmcaster \
+		com.yubico.yubioath \
+		de.shorsh.discord-screenaudio \
+		in.cinny.Cinny \
+		io.github.arunsivaramanneo.GPUViewer \
+		im.riot.Riot \
+		im.srain.Srain \
+		org.signal.Signal \
+		org.gnome.Keysign \
+		org.kde.kdenlive \
+		io.qt.Designer \
+		org.x.Warpinator \
+		tv.plex.PlexDesktop \
+		org.jdownloader.JDownloader \
+		org.gnome.baobab \
+		org.videolan.VLC \
+		org.thentrythis.Samplebrain \
+		com.authy.Authy \
+		com.github.tchx84.Flatseal \
+		md.obsidian.Obsidian
 }
 
 DISTRO=$(_getdistro)
@@ -60,6 +115,11 @@ else
     echo "Unsupported Distro"
 fi
 
+if command -v flatpak &> /dev/null
+then
+    _install_flatpak
+fi
+
 echo Copying Home Folder
-find ./linux/home/kate/ -maxdepth 1 -regex '^\./.*' -exec /usr/bin/cp -rf {} ~/ \;
+find ./linux/user_home_folder/ -maxdepth 1 -regex '^\./.*' -exec /usr/bin/cp -rf {} ~/ \;
 echo "Run \"source ~/.zshrc\" to load"
