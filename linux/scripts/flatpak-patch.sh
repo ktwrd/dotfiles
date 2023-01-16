c6cefb662bbfb133885c061dd2e5385d0e1b8db6
kate <kate@dariox.club>
2023-01-16 11:19:18 +0800

    update flatpak-patch.sh

--- linux/scripts/flatpak-patch.sh
+++ linux/scripts/flatpak-patch.sh
@@ -9,10 +9,23 @@ function discordPatch_sandboxed()
 {
 	ln -sf "$XDG_RUNTIME_DIR/{app/com.discordapp.Discord,}/discord-ipc-0"
 	# Support for vscode rich presence
-	sudo flatpak override com.visualstudio.code --filesystem=$XDG_RUNTIME_DIR/app/com.discordapp.Discord/
+	flatpak override com.visualstudio.code -u --filesystem=$XDG_RUNTIME_DIR/app/com.discordapp.Discord/
+	flatpak override com.visualstudio.code -u --filesystem=$XDG_RUNTIME_DIR
+}
+
+function plexPatch()
+{
+	# Allow GPU access and wayland
+	flatpak override tv.plex.PlexDesktop -u --sockets=wayland
+	flatpak override tv.plex.PlexDesktop -u --devices=dri
 }
 
 # Patch discord rich presence to work with
 # flatpak applications, sandboxed and
 # non-sandboxed.
 discordPatch; discordPatch_sandboxed
+
+plexPatch;
+
+# allow GPU access
+flatpak override com.github.iwalton3.jellyfin-media-player -u --devices=dri
