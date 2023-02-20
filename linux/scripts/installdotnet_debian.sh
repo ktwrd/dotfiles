ac5d73b48acc3c556b320fda6a23553fa2a881ce
kate <kate@dariox.club>
2023-02-20 12:17:57 +0800

    Add script to install dotnet for debian

---
+++ linux/scripts/installdotnet_debian.sh
@@ -0,0 +1,15 @@
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+# Install
+/usr/bin/apt update
+/usr/bin/apt install -y \
+    dotnet-sdk-6.0 \
+    dotnet-sdk-7.0 \
+    dotnet-runtime-6.0 \
+    dotnet-runtime-7.0 \
+    aspnetcore-runtime-6.0 \
+    aspnetcore-runtime-7.0 \
+    aspnetcore-targeting-pack-6.0 \
+    aspnetcore-targeting-pack-7.0 \
