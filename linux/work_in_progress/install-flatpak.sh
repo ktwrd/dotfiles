9cbdcde95c655a3b7596fce855ae3379b3fbb51e
kate <kate@dariox.club>
2026-01-06 09:26:57 +0800

    Created linux/work_in_progress/install-flatpak.sh

---
+++ linux/work_in_progress/install-flatpak.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -xeu -o pipefail
+flatpak install -y flathub \
+    org.videolan.VLC \
+    org.sqlitebrowser.sqlitebrowser \
+    org.scintilla.SciTE \
+    org.onlyoffice.desktopeditors \
+    org.prismlauncher.PrismLauncher \
+    org.qbittorrent.qBittorrent \
+    org.kde.kdenlive \
+    org.mozilla.Thunderbird \
+    org.inkscape.Inkscape \
+    org.gnome.meld \
+    org.gimp.GIMP \
+    org.filezillaproject.Filezilla \
+    org.chromium.Chromium \
+    com.yubico.yubioath \
+    fr.romainvigier.MetadataCleaner \
+    io.dbeaver.DBeaverCommunity \
+    io.missioncenter.MissionCenter \
+    md.obsidian.Obsidian \
+    net.agalwood.Motrix \
+    com.obsproject.Studio \
+    com.parsecgaming.parsec \
+    com.usebottles.bottles \
+    com.valvesoftware.Steam \
+    com.github.tchx84.Flatseal \
+    com.jgraph.drawio.desktop \
+    com.boxy_svg.BoxySVG
